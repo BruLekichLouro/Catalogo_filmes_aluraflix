718017ebc1b1f90210ef6033486ec738cf937a47
BruLekichLouro <bububio@yahoo.com.br>
2023-11-17 14:40:00 -0300

    Iniciando projeto Catálogo de filmes

---
+++ main.js
@@ -0,0 +1,4 @@
+var filme1 = 'https://m.media-amazon.com/images/S/pv-target-images/1083b948613dcbc0aaacf28a83baa4db4d1cba4c2a7c94aa7639e7775ad37d29.jpg'
+var filme2 = 'https://upload.wikimedia.org/wikipedia/pt/thumb/1/1b/Schoolrockposter.jpg/210px-Schoolrockposter.jpg'
+
+document.getElementById("filme1").innerHTML= "<img src="+ filme1 + ">";
