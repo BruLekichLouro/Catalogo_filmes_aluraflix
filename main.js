f49e4a122b0708a99359c0cd97be6680b1bda6c7
BruLekichLouro <bububio@yahoo.com.br>
2023-11-18 12:58:08 -0300

    Ajustes finos

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-var filme1 = 'https://m.media-amazon.com/images/S/pv-target-images/1083b948613dcbc0aaacf28a83baa4db4d1cba4c2a7c94aa7639e7775ad37d29.jpg'
-var filme2 = 'https://upload.wikimedia.org/wikipedia/pt/thumb/1/1b/Schoolrockposter.jpg/210px-Schoolrockposter.jpg'
+var listFilms=['https://m.media-amazon.com/images/S/pv-target-images/1083b948613dcbc0aaacf28a83baa4db4d1cba4c2a7c94aa7639e7775ad37d29.jpg','https://upload.wikimedia.org/wikipedia/pt/thumb/1/1b/Schoolrockposter.jpg/210px-Schoolrockposter.jpg',"https://upload.wikimedia.org/wikipedia/pt/7/76/10_Things_I_Hate_About_You.jpg"];
 
-document.getElementById("filme1").innerHTML= "<img src="+ filme1 + ">";
+for(var i=0; i < listFilms.length; i++){
+    document.write( "<img src="+ listFilms[i] + ">");
+}
